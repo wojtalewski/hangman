f4851ec0c3c57ed0c666e72c4ab2403775ca895c
wojtalewski <92038898+wojtalewski@users.noreply.github.com>
2021-11-24 15:03:59 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -3,7 +3,7 @@
 const puzzleEl = document.querySelector(`#puzzle`)
 const guessesEl = document.querySelector(`#guesses`)
 
-const game1 = new Hangman(`Cat`, 2)
+const game1 = new Hangman(`Ruchanie`, 4)
 
 puzzleEl.textContent = game1.getPuzzle()
 guessesEl.textContent = game1.getStatus()
