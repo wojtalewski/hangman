4b7916bfb72e29ce761c66fc8d78e14d4ff03ac1
wojtalewski <92038898+wojtalewski@users.noreply.github.com>
2021-11-24 14:57:35 +0100

    Add files via upload

---
+++ hangman.js
@@ -0,0 +1,62 @@
+'use strict'
+
+class Hangman {
+    constructor(word, remainingGuesses) {
+        this.word = word.toLowerCase().split(``)
+        this.guessedLetters = []
+        this.remainingGuesses = remainingGuesses
+        this.status = `Playing`
+    }
+    calculateStatus() {
+        const finished = this.word.every((letter) => this.guessedLetters.includes(letter))
+
+        if (this.remainingGuesses === 0) {
+            this.status = `Failed`
+        } else if (finished) {
+            this.status = `Finished`
+        } else {
+            this.status = `Playing`
+        }
+    }
+    getStatus() {
+        if (this.status === `Playing`) {
+            return `Guesses left: ${this.remainingGuesses}`
+        } else if (this.status === `Failed`) {
+            return `Nice try! The word was "${this.word.join(``)}"`
+        } else {
+            return `Great work! you guessed the word!`
+        }
+    }
+    makeGuess(guess) {
+        guess = guess.toLowerCase()
+        const isUnique = !this.guessedLetters.includes(guess)
+        const isBadGuess = !this.word.includes(guess)
+    
+        if (this.status !== `Playing`) {
+            return 
+        }
+
+        if (isUnique) {
+            this.guessedLetters.push(guess)    
+        } 
+        
+        if (isUnique && isBadGuess) {
+            this.remainingGuesses--
+        } 
+    
+        this.calculateStatus()
+    }
+    getPuzzle() {
+        let puzzle = ``
+
+        this.word.forEach((letter) => {
+            if (this.guessedLetters.includes(letter) || letter === ` `) {
+                puzzle += letter
+            } else {
+                puzzle += `*`
+            }
+        })
+        return puzzle 
+    }
+}
+
